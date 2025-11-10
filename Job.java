e62a85f23e20157439eaf1cf7f8241a3c68c355b
D Ward <dw716@exeter.ac.uk>
2025-11-10 15:47:27 +0000

    Implement Job class for concurrent processing

---
+++ Job.java
@@ -0,0 +1,22 @@
+public class Job implements Runnable {
+    private final int jobNumber;
+
+    public Job(int jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Job " + jobNumber + " is being processed by thread: "
+                + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(1000); // simulate work
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // restore interrupt status
+        }
+
+        System.out.println("Job " + jobNumber + " is ending in thread: "
+                + Thread.currentThread().getName());
+    }
+}
